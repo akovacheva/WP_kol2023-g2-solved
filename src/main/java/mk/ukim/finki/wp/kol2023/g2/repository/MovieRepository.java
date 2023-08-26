b90a20333d722124eaa0da4d4fd25e870c1771cc
akovacheva <anna_kovacheva@hotmail.com>
2023-08-26 16:33:47 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/wp/kol2023/g2/repository/MovieRepository.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.kol2023.g2.repository;
+
+import mk.ukim.finki.wp.kol2023.g2.model.Genre;
+import mk.ukim.finki.wp.kol2023.g2.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findByGenre(Genre genre);
+    List<Movie> findByGenreAndRatingLessThan(Genre genre, Double rating);
+    List<Movie> findByRatingLessThan(Double rating);
+}
